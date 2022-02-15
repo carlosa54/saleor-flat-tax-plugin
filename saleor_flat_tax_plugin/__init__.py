2f503e96ff8a4bc8812bf3cf12476e3a8b3c2692
Carlos Ramírez <carlosarg54@gmail.com>
2022-02-14 18:16:34 -0700

    Add precision (#2)
    * Prevent long float numbers
    
    * Add flat_tax with precision
    
    * Bump version

--- saleor_flat_tax_plugin/__init__.py
+++ saleor_flat_tax_plugin/__init__.py
@@ -1,5 +1,5 @@
 from decimal import Decimal
-from prices import Money, MoneyRange, TaxedMoney, TaxedMoneyRange, flat_tax
+from prices import Money, MoneyRange, TaxedMoney, TaxedMoneyRange
 
 META_CODE_KEY = "flattax.code"
 META_DESCRIPTION_KEY = "flattax.description"
@@ -38,6 +38,31 @@ def apply_tax_to_price(taxes, rate_name, base):
     return tax_to_apply(base, keep_gross=keep_gross)
 
 
+# Taken from prices.flat_tax but adding precision
+def flat_tax(base, tax_rate, *, keep_gross=False, precision=Decimal(".0001")):
+    """Apply a flat tax by either increasing gross or decreasing net amount."""
+    fraction = Decimal(1) + tax_rate
+    if isinstance(base, (MoneyRange, TaxedMoneyRange)):
+        return TaxedMoneyRange(
+            flat_tax(base.start, tax_rate, keep_gross=keep_gross),
+            flat_tax(base.stop, tax_rate, keep_gross=keep_gross))
+    if isinstance(base, TaxedMoney):
+        if keep_gross:
+            new_net = (base.net / fraction).quantize(precision)
+            return TaxedMoney(net=new_net, gross=base.gross)
+        else:
+            new_gross = (base.gross * fraction).quantize(precision)
+            return TaxedMoney(net=base.net, gross=new_gross)
+    if isinstance(base, Money):
+        if keep_gross:
+            net = (base / fraction).quantize(precision)
+            return TaxedMoney(net=net, gross=base)
+        else:
+            gross = (base * fraction).quantize(precision)
+            return TaxedMoney(net=base, gross=gross)
+    raise TypeError('Unknown base for flat_tax: %r' % (base,))
+
+
 def get_tax_rate_by_name(rate_name, taxes=None):
     """Return value of tax rate for current taxes."""
     if not taxes or not rate_name:
@@ -63,7 +88,7 @@ def get_tax_for_rate(tax_rates, rate_name=DEFAULT_TAX_RATE_NAME):
     if rate is None:
         return None
 
-    final_tax_rate = Decimal(float(rate) / 100)
+    final_tax_rate = Decimal(rate) / 100
 
     def tax(base, keep_gross=False):
         return flat_tax(base, final_tax_rate, keep_gross=keep_gross)
